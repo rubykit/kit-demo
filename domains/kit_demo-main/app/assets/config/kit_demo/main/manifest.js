33ed0bcd3b86458142a0a9b5a947f85efb468ea5
Nathan Appere <nathan.appere@gmail.com>
2021-09-01 12:07:14 +0200

    Now uses FaviconComponent

--- domains/kit_demo-main/app/assets/config/kit_demo/main/manifest.js
+++ domains/kit_demo-main/app/assets/config/kit_demo/main/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../../../images
 
+//= link kit_demo/main/favicon/site.webmanifest
+//= link kit_demo/main/favicon/browserconfig.xml
+
 //= link_directory ../../../javascripts .js
 //= link_directory ../../../stylesheets .css
